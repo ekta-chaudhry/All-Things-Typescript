36d3c5a5bc31baaa6deaed7073c9e5586fb796d2
Ekta Chaudhry <ekta.chaudhry0701@gmail.com>
2025-03-26 20:33:06 +0530

    illustrated interfaces in Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-const greet = (name: string) => `Hello, ${name}!`;
+interface User {
+    name: string;
+    age: number;
+    address?: {
+        city: string;
+        street: string;
+    }
+}
+
+const user: User = {
+    name: 'John',
+    age: 30
+}
+
+function isLegal(user: User): boolean {
+    return user.age >= 18;
+}
+
+isLegal(user);
